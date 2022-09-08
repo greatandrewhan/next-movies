d39b3e104d4188ae8ab8e15028ee6691c9c502d1
Sung Kil Han <90734610+greatandrewhan@users.noreply.github.com>
2022-09-07 23:45:58 -0400

    vercel api

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home({ results }) {
 
 export async function getServerSideProps() {
   const { results } = await (
-    await fetch(`http://localhost:3000/api/movies`)
+    await fetch(`https://next-movies-psi.vercel.app/api/movies`)
   ).json();
   return {
     props: {
